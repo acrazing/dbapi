452f1d2828b8b77ebc246b782ae3988184dd2481
acrazing <joking.young@gmail.com>
2017-04-28 01:33:47 +0800

    fix(core): remove useless test code

--- dbapi/DoubanAPI.py
+++ dbapi/DoubanAPI.py
@@ -94,52 +94,19 @@ class DoubanAPI(object):
         return Group(self.headers, self.cookies, self.user_alias)
 
 
-def test_login(argv):
-    api = DoubanAPI()
-    api.login(argv[0], argv[1])
-    print('login with username: %s, password: %s' % (api.username, api.password))
-    print('user home url: %s' % api.user_alias)
-    print('cookies', api.cookies)
-
-
-def test_persist(argv):
-    api = DoubanAPI()
-    cookies = {}
-    [cookies.update((c.split('=', 1),)) for c in argv[0].split('; ')]
-    api.use(cookies)
-    print('used: alias: %s' % api.user_alias)
-    api2 = DoubanAPI()
-    print('load cookies', api2.cookies)
-    r2 = requests.get(API_ACCOUNT_HOME, cookies=api2.cookies, headers=api2.headers)
-    print('load result: %s' % r2.url)
-
-
-def test_argv(argv):
-    origin = "ll=\"108296\"; bid=78vrfvr1mMU; _ga=GA1.2.897983712.1493266936; ap=1; " \
-             "_vwo_uuid_v2=47B9B09B65CC50A3B87593B27E72A38F|0e509048565e477027bd2f7b15eeebd1; " \
-             "__utma=30149280.897983712.1493266936.1493266936.1493266936.1; __utmb=30149280.11.10.1493266936; " \
-             "__utmz=30149280.1493266936.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=30149280.6101; " \
-             "ck=fLns; _pk_id.100001.8cb4=75ccca2b64c8e7b4.1493266929.1.1493272150.1493266929.; " \
-             "_pk_ses.100001.8cb4=*; push_noty_num=0; push_doumail_num=0 "
-    arg = argv[0]
-    print('equals:', arg == origin)
-    r1 = requests.get(API_ACCOUNT_HOME, headers={'Cookie': arg})
-    print('r1.url:', r1.url)
-    r2 = requests.get(API_ACCOUNT_HOME, headers={'Cookie': origin})
-    print('r2.url:', r2.url)
-
-
-def test_first_api(argv):
-    api = DoubanAPI()
-    print('searched groups:', api.group.search_groups('group'))
-
-
 def test_api(argv):
     api = DoubanAPI()
     print('testing: %s.%s' % (argv[0], argv[1]))
     app = getattr(api, argv[0])
     func = getattr(app, argv[1])
     pprint(func(*argv[2:]))
+
+
+def test_client(argv):
+    api = DoubanAPI()
+    print('testing: %s' % argv[0])
+    func = getattr(api, argv[0])
+    pprint(func(*argv[1:]))
 
 
 if __name__ == '__main__':
