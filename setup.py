08f7f40bb896c126832f7459683152db9dba7c18
acrazing <joking.young@gmail.com>
2017-04-28 14:21:19 +0800

    doc(README): update readme for pypi

--- setup.py
+++ setup.py
@@ -6,15 +6,19 @@
 # File: setup.
 from setuptools import setup
 
+with open('./README.md') as f:
+    desc = f.read()
+
 setup(
     name='dbapi',
-    version='0.0.1',
+    version='0.0.2',
     description='基于爬虫的豆瓣API SDK',
     url='https://github.com/acrazing/dbapi',
     author='acrazing',
     author_email='joking.young@gmail.com',
     license='MIT',
     keywords='豆瓣 douban 豆瓣小组 api',
+    long_description=desc,
     install_requires=['lxml', 'requests'],
     packages=['dbapi'],
 )
