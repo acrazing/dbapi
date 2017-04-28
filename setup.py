b0408956fdeb7a372497e465b8ce46b894a5264c
acrazing <joking.young@gmail.com>
2017-04-28 13:43:23 +0800

    feat(core): upload to pypi

--- setup.py
+++ setup.py
@@ -4,3 +4,17 @@
 # License: MIT.
 # Author: acrazing <joking.young@gmail.com>.
 # File: setup.
+from setuptools import setup
+
+setup(
+    name='dbapi',
+    version='0.0.1',
+    description='基于爬虫的豆瓣API SDK',
+    url='https://github.com/acrazing/dbapi',
+    author='acrazing',
+    author_email='joking.young@gmail.com',
+    license='MIT',
+    keywords='豆瓣 douban 豆瓣小组 api',
+    install_requires=['lxml', 'requests'],
+    packages=['dbapi'],
+)
