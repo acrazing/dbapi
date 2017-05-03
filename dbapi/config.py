db2d8d9135a26aa8da1adcc5f38069be5d7b0f16
acrazing <joking.young@gmail.com>
2017-05-03 23:33:59 +0800

    fix(relation): the structure of test relation spider

--- dbapi/config.py
+++ dbapi/config.py
@@ -17,9 +17,12 @@ api_config = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate, sdch, br',
         'Accept-Language': 'zh,zh-CN;q=0.8,zh-TW;q=0.6,en;q=0.4,en-US;q=0.2',
+        'Cache-Control': 'max-age=0',
         'Connection': 'closed',
+        'Upgrade-Insecure-Requests': '1',
+        'Referer': 'https://www.douban.com/',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/58.0.3029.81 Safari/537.37',
+                      'Chrome/58.0.3029.81 Safari/537.36',
     },
     'logger': 'dbapi',
     'log_level': logging.DEBUG,
--- test/relation.py
+++ test/relation.py
@@ -5,70 +5,121 @@
 # Author: acrazing <joking.young@gmail.com>.
 # File: relation.
 import atexit
+import gc
 import json
+import logging
 import os
+import sys
 import time
-import traceback
 from sys import argv
 from threading import Thread
+
+import psutil
 
 from dbapi.DoubanAPI import DoubanAPI
 from dbapi.endpoints import API_PEOPLE_HOME
 
 
-class Worker(Thread):
-    """
-    进行爬虫任务的Worker
-    """
-
-    def __init__(self, api, spider):
+class Worker:
+    def __init__(self, username, password=None, cookies=None, spider=None):
         """
-        :type api: DoubanAPI
-        :param api:  初始化过的API客户端
-        
+        :param username: 
+        :param password: 
+        :param cookies:
+         
         :type spider: RelationSpider
         :param spider: 
         """
-        super(Worker, self).__init__()
-        self._api = api
-        self._spider = spider
-        self.setDaemon(True)
+        self.id = username
+        api = DoubanAPI(logger=username, persist_file='__cache__' + username + '.json')
+        if api.user_alias is None:
+            if cookies:
+                api.use(cookies)
+            if api.user_alias is None and password:
+                api.login(username, password)
+        time.sleep(1)
+        self.api = api
+        self.spider = spider
+        self.parsing = None
+        self.killing = False
+
+        self.thread = None
+        """:type: Thread"""
 
     def run(self):
         while True:
-            if not self._spider.running:
-                return
-            alias = self._spider.next_alias()
-            if not alias:
-                self._spider.sleep(self)
-                return
-            self._api.logger.debug('spider start parse user<%s>' % alias)
+            if self.spider.running is False:
+                return self.stop('spider stopped')
+            if self.killing is True:
+                return self.stop('worker killed')
+            alias = self.spider.next_alias()
+            if alias is None:
+                return self.stop('no more user to parse')
+            self.parsing = alias
+            self.api.logger.debug('spider start parse user<%s>' % alias)
             next_start = 0
             try:
                 while next_start is not None:
-                    contacts = self._api.people.list_contacts(alias, next_start)
+                    if self.spider.running is False:
+                        self.spider.reset_alias(alias)
+                        return self.stop('spider stopped when parse %s[%s]' % (alias, next_start))
+                    if self.killing is True:
+                        self.spider.reset_alias(alias)
+                        return self.stop('worker killed when parse %s[%s]' % (alias, next_start))
+                    contacts = self.api.people.list_contacts(alias, next_start)
                     next_start = contacts['next_start']
                     for item in contacts['results']:
-                        if self._spider.has_alias(item['alias']):
+                        if self.spider.has_alias(item['alias']):
                             continue
+                        if self.spider.running is False:
+                            self.spider.reset_alias(alias)
+                            return self.stop('spider stopped when parse %s, %s' % (alias, item['alias']))
+                        if self.killing is True:
+                            self.spider.reset_alias(alias)
+                            return self.stop('worker killed when parse %s, %s' % (alias, item['alias']))
                         try:
-                            user = self._api.people.get_people(item['alias'])
-                            if not user:
-                                self._spider.report_alias(item['alias'])
-                            elif self._spider.add_user(user):
-                                self._api.logger.debug(
+                            user = self.api.people.get_people(item['alias'])
+                            if user is None:
+                                self.spider.report_alias(item['alias'])
+                            elif self.spider.add_user(user):
+                                self.api.logger.debug(
                                     'add user<%s> contact_count<%s> rev_contact_count<%s>'
                                     % (user['alias'], user['contact_count'], user['rev_contact_count']))
                         except Exception as e:
-                            print(traceback.print_exc())
-                            self._api.logger.error('get user <%s> error: %s' % (item['alias'], e))
+                            self.api.logger.exception('get user <%s> error: %s' % (item['alias'], e))
                         time.sleep(3)  # 防验证码
-                self._spider.done_alias(alias)
+                self.spider.done_alias(alias)
                 time.sleep(3)
             except Exception as e:
-                self._api.logger.error('parse user <%s> error: %s' % (alias, e))
+                self.api.logger.exception('parse user <%s> error: %s' % (alias, e))
                 # 不能在循环中捕获，虽然更高效，但是可能会死循环
-                self._spider.reset_alias(alias)
+                self.spider.reset_alias(alias)
+
+    @property
+    def running(self):
+        return self.thread is not None and self.thread.is_alive()
+
+    def stop(self, reason, join=False):
+        if self.thread is None:
+            return
+        self.api.logger.debug('worker thread <%s> with <%s> stopped for <%s>' % (self.thread.ident, self.id, reason))
+        self.killing = True
+        join and self.thread.join()
+        self.thread = None
+        self.killing = False
+
+    def start(self):
+        self.thread = Thread(target=self.run, daemon=True)
+        self.thread.start()
+        self.api.logger.debug('worker thread <%s> with <%s> started' % (self.thread.ident, self.id))
+
+    def reload(self):
+        self.stop('reload', True)
+        self.start()
+
+
+def mb(value):
+    return value / 1024 / 1024
 
 
 class RelationSpider(object):
@@ -77,56 +128,134 @@ class RelationSpider(object):
     """
 
     def __init__(self, **kwargs):
-        self._persist_file = kwargs.get('persist_file', '__relation__.json')  # 持久化文件
-        self._persist_step = kwargs.get('persist_step', 100)  # 爬N个用户后持久化一次
-        self._result_file = kwargs.get('result_file', '__result__.json')  # 储存分析结果的文件
-        self._min_rev = int(kwargs.get('min_rev', 10000))  # 关注者最小值
-        init_users = kwargs.get('init_users', '')  # 种子用户，只有用户ID
-        self._init_users = init_users.split(',') if init_users else []
-        self._users_list = []  # 过滤出来的需要继续爬的用户列表，只保存ID，pop和append都是安全的
-        self._users_dict = {}  # 爬过的用户字典，需要保证在persist时不被写入(所以在运行中persist有问题！)
-        self._parsing = []  # 正在被爬关注用户的ID列表，append和remove是线程安全的(remove确定吗?)
-        self._reports_dict = {}  # 爬取出错的用户
-        self._step = 0  # 上一次持久化后添加的用户数，不需要关注并发，误差可以忽略
-        self.running = False
-        self._persisting = False  # 赋值操作具有原子性，可保线程安全，保证在persist时user_dict不被写入
-        with open(kwargs.get('accounts_file', 'accounts.dat'), 'r') as f:
-            accounts = [item.strip().split(':', 2) for item in f.read().split('\n') if item]
-        if not accounts:
-            raise Exception('must set accounts for spider')
-        api_list = []
-        workers = []  # 每个账户一个爬虫
-        aliases = []
-        for item in accounts:
-            api = DoubanAPI(cfg={
-                'logger': item[0],
-                'persist_file': '__cache__' + item[0] + '.json',
-            })
-            time.sleep(1)
-            if not api.user_alias:
-                if len(item) > 2:
-                    api.use(item[2])
-                else:
-                    api.login(item[0], item[1])
-            if api.user_alias:
-                aliases.append(item[0])
-                api_list.append(api)
-                workers.append(Worker(api, self))
-        self.load()
-        self._api = api_list[0]
-        self._api.logger.info('spider start with accounts<%s>' % aliases)
-        self._workers = workers
-        self._sleep_workers = []
-        self.init()
-        atexit.register(self.abort)
+        """持久化文件"""
+        self.persist_file = kwargs.get('persist_file', '__relation__.json')
+        """:type: str"""
 
-    def abort(self):
+        """爬N个用户后持久化一次"""
+        self.persist_step = kwargs.get('persist_step', 100)
+        """:type: int"""
+
+        """储存分析结果的文件"""
+        self.result_file = kwargs.get('result_file', '__result__.json')
+        """:type: str"""
+
+        """关注者最小值"""
+        self.min_rev = int(kwargs.get('min_rev', 10000))
+        """:type: int"""
+
+        init_users = kwargs.get('init_users')
+        """:type: str"""
+        """种子用户，只有用户ID"""
+        self.init_users = init_users.split(',') if init_users else []
+        """:type: dict[str]"""
+
+        """过滤出来的需要继续爬的用户列表，只保存ID，pop和append都是安全的"""
+        self.users_list = set()
+        """:type: set[str]"""
+
+        """爬过的用户字典，需要保证在persist时不被写入(所以在运行中persist有问题！)"""
+        self.users_dict = {}
+        """:type: dict[dict]"""
+
+        """爬取出错的用户"""
+        self.reports_dict = {}
+        """:type: dict[str, dict]"""
+
+        self.step = 0
+        """:type: int"""
+
+        """是否已开始"""
+        self.running = False
+        """:type: bool"""
+
+        """赋值操作具有原子性，可保线程安全，保证在persist时user_dict不被写入"""
+        self.persisting = False
+        """:type: bool"""
+
+        logger = logging.getLogger('spider:relation')
+        if len(logger.handlers) is 0:
+            handler = logging.StreamHandler(sys.stderr)
+            formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+            logger.setLevel(logging.DEBUG)
+        self.logger = logger
+
+        """爬虫表"""
+        self.workers = []
+        """:type: list[Worker]"""
+
+        with open(kwargs.get('accounts_file', 'accounts.dat'), 'r') as f:
+            [self.add_worker(*item.strip().split(':', 2)) for item in f.read().split('\n') if item]
+
+        self.load()
+
+        atexit.register(self.stop)
+
+    def add_worker(self, username, password=None, cookies=None):
+        worker = Worker(username, password, cookies, self)
+        if worker.api.user_alias is not None:
+            self.workers.append(worker)
+
+    def should_parse(self, user):
+        """
+        :type user: dict
+        :param user: 
+        
+        :return: 
+        """
+        return self.min_rev < user['rev_contact_count'] and not user.get('parsed', False)
+
+    def load(self):
+        if os.path.isfile(self.persist_file):
+            with open(self.persist_file, 'r') as f:
+                data = json.load(f) or {}
+                self.users_dict = data.get('users_dict', {})
+                self.reports_dict = data.get('reports_dict', {})
+        [self.users_list.add(alias) for (alias, user) in self.users_dict.items() if self.should_parse(user)]
+        [self.users_list.add(alias) for alias in self.init_users if
+         alias not in self.users_dict and alias not in self.reports_dict]
+
+    def persist(self):
+        self.persisting = True
+
+        def persist():
+            with open(self.persist_file, 'w+') as f:
+                self.logger.info('spider persist data, size: %s' % len(self.users_dict))
+                json.dump({
+                    'users_dict': self.users_dict,
+                    'reports_dict': self.reports_dict,
+                }, f, indent=2)
+            with open(self.result_file, 'w+') as f:
+                json.dump(self.summary(), f, indent=2)
+
+        thread = Thread(target=persist)
+        thread.start()
+        thread.join()
+        self.persisting = False
+
+    def start(self):
+        if len(self.workers) is 0:
+            raise Exception('Cannot start spider without worker')
+        self.running = True
+        [item.start() for item in self.workers]
+        ps = psutil.Process(os.getpid())
+        while True:
+            time.sleep(1800)  # half hour
+            rss = ps.memory_info().rss
+            if rss > 1 << 25:
+                [item.reload() for item in self.workers]
+                gc.collect()
+                self.logger.warning('memory overflow, before is <%s>, after is <%s>'
+                                    % (mb(rss), mb(ps.memory_info().rss)))
+
+    def stop(self):
         if not self.running:
             return
         self.running = False
-        self._api.logger.info('spider aborted when parsing<%s>' % self._parsing)
-        while self._parsing:
-            self.reset_alias(self._parsing.pop())
+        self.logger.info('spider aborted when parsing<%s>' % [item.parsing for item in self.workers])
+        [self.reset_alias(item.parsing) for item in self.workers]
         self.persist()
 
     def add_user(self, user):
@@ -140,35 +269,33 @@ class RelationSpider(object):
         :rtype: bool
         :return: 
         """
-        if self._persisting or not user or user['alias'] in self._users_dict:
+        if self.persisting or not user or user['alias'] in self.users_dict:
             return False
         user['parsed'] = False
-        self._users_dict[user['alias']] = user
-        if user['rev_contact_count'] > self._min_rev:
-            self._users_list.append(user['alias'])
+        self.users_dict[user['alias']] = user
+        if self.should_parse(user):
+            self.users_list.add(user['alias'])
             self.wake_up()
-        self._step += 1
-        if self._step > self._persist_step:
+        self.step += 1
+        if self.step > self.persist_step:
             self.persist()
-            self._step = 0
+            self.step = 0
         return True
 
     def wake_up(self):
-        if self.running and self._sleep_workers:
-            self._sleep_workers.pop(0).start()
-
-    def sleep(self, worker):
-        self._workers.append(worker)
+        for item in self.workers:
+            if item.running is False:
+                item.start()
 
     def report_alias(self, alias):
-        if alias not in self._reports_dict:
-            self._reports_dict[alias] = {
+        if alias not in self.reports_dict:
+            self.reports_dict[alias] = {
                 'count': 1,
                 'alias': alias,
                 'url': API_PEOPLE_HOME % alias,
             }
         else:
-            self._reports_dict[alias]['count'] += 1
+            self.reports_dict[alias]['count'] += 1
 
     def has_alias(self, alias):
         """
@@ -180,83 +307,29 @@ class RelationSpider(object):
         :rtype: bool
         :return: 
         """
-        return alias in self._users_dict or (alias in self._reports_dict and self._reports_dict[alias]['count'] > 1)
+        return alias in self.users_dict or (alias in self.reports_dict and self.reports_dict[alias]['count'] > 0)
 
     def reset_alias(self, alias):
-        if alias in self._users_dict:
-            self._users_dict[alias]['parsed'] = False
-            if alias not in self._users_list:
-                self._users_list.append(alias)
-        if alias in self._parsing:
-            self._parsing.remove(alias)
+        if alias in self.users_dict:
+            self.users_dict[alias]['parsed'] = False
+            if alias not in self.users_list:
+                self.users_list.add(alias)
 
     def next_alias(self):
-        if not self._users_list:
+        if len(self.users_list) is 0:
             return None
-        alias = self._users_list.pop(0)
-        self._parsing.append(alias)
+        alias = self.users_list.pop()
         return alias
 
     def done_alias(self, alias):
-        if alias in self._users_dict:
-            self._users_dict[alias]['parsed'] = True
-        if alias in self._parsing:
-            self._parsing.remove(alias)
-
-    def init(self):
-        for alias, user in self._users_dict.items():
-            if user['rev_contact_count'] > self._min_rev and not user.get('parsed'):
-                self._users_list.append(alias)
-        for alias in self._init_users:
-            if alias not in self._users_dict:
-                try:
-                    user = self._api.people.get_people(alias)
-                    self.add_user(user)
-                    if self._users_list[-1] == alias:
-                        self._users_list.pop()
-                        self._users_list.insert(0, alias)
-                    time.sleep(2)
-                except Exception as e:
-                    self._api.logger.error('spider init user<%s> error: %s' % (alias, e))
-        self._api.logger.debug('spider init with users<%s>, users to parse <%s>'
-                               % (self._init_users, len(self._users_list)))
-
-    def load(self):
-        if os.path.isfile(self._persist_file):
-            with open(self._persist_file, 'r') as f:
-                data = json.load(f) or {}
-                self._users_dict = data.get('users_dict', {})
-                self._reports_dict = data.get('reports_dict', {})
-
-    def persist(self):
-        self._persisting = True
-
-        def persist():
-            with open(self._persist_file, 'w+') as f:
-                self._api.logger.info('spider persist data, size: %s' % len(self._users_dict))
-                json.dump({
-                    'users_dict': self._users_dict,
-                    'reports_dict': self._reports_dict,
-                }, f, indent=2)
-            with open(self._result_file, 'w+') as f:
-                json.dump(self.summary(), f, indent=2)
-
-        thread = Thread(target=persist)
-        thread.start()
-        thread.join()
-        self._persisting = False
-
-    def start(self):
-        self.running = True
-        [worker.start() for worker in self._workers]
-        while True:
-            time.sleep(3)
+        if alias in self.users_dict:
+            self.users_dict[alias]['parsed'] = True
 
     def summary(self):
-        total = len(self._users_dict)
+        total = len(self.users_dict)
         results = []
-        for alias, user in self._users_dict.items():
-            if user['rev_contact_count'] > self._min_rev:
+        for alias, user in self.users_dict.items():
+            if user['rev_contact_count'] > self.min_rev:
                 results.append(user)
         results.sort(key=lambda item: item['rev_contact_count'], reverse=True)
         return {'results': results, 'total': total, 'filtered': len(results)}
